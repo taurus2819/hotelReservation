f8a7f947258c6cb42e3f434062815d55237c3c83
Prashanth Sitikond <taurus2819@gmail.com>
2022-04-03 20:47:26 +1200

    debug mssgs added

--- src/HotelReservationApp.java
+++ src/HotelReservationApp.java
@@ -4,5 +4,6 @@ public class HotelReservationApp {
     public static void main(String[] args) {
         System.out.println("Welcome to the Hotel Reservation Application");
         MainMenu.getMainMenu();
+        System.out.println("out of main menu");
     }
 }
